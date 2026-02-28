e3e4f748c167634c2096869090a484f991f32bf9
deepinder-gill <deepinderrgilll@gmail.com>
2026-03-01 03:23:50 +0530

    Update config.py

--- core/config.py
+++ core/config.py
@@ -1,3 +1,6 @@
 from typing import List
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings
 from pydantic import field_validator
+
+class Settings(BaseSettings):
+    API_PREFIX: str = "/api/v1"
