0d45e671a5f52b20e89a45567c1d24924cf23a6c
deepinder-gill <deepinderrgilll@gmail.com>
2026-03-03 03:28:00 +0530

    Update config.py

--- core/config.py
+++ core/config.py
@@ -4,7 +4,7 @@ from pydantic import field_validator
 
 class Settings(BaseSettings):
     API_PREFIX: str = "/api/v1"
-    
+
     DATABASE_URL: str
 
     DEBUG: bool = False
@@ -12,3 +12,14 @@ class Settings(BaseSettings):
     ALLOWED_ORIGINS: str = ""
 
     OPEN_API_KEY: str = ""
+
+    @field_validator("ALLOWED_ORIGINS")
+    def allowed_origins_validator(cls, v: str) -> List[str]:
+        return v.split(",") if v else []
+
+class Config:
+    env_file = ".env"
+    enf_file_encoding = "utf-8"
+    case_sensitive = True
+
+    
