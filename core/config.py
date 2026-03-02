fd6fa74bb674176021d8c41bf0fe92d2277f0347
deepinder-gill <deepinderrgilll@gmail.com>
2026-03-03 03:22:40 +0530

    Update config.py

--- core/config.py
+++ core/config.py
@@ -4,4 +4,11 @@ from pydantic import field_validator
 
 class Settings(BaseSettings):
     API_PREFIX: str = "/api/v1"
+    
+    DATABASE_URL: str
 
+    DEBUG: bool = False
+
+    ALLOWED_ORIGINS: str = ""
+
+    OPEN_API_KEY: str = ""
