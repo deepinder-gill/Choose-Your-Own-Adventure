60c18fa9bceff3d2f1515cbe83a92a84f2924d4f
deepinder-gill <deepinderrgilll@gmail.com>
2026-03-07 03:40:56 +0530

    Update story.py

--- routers/story.py
+++ routers/story.py
@@ -3,6 +3,8 @@ from typing import Optional
 from datetime import datetime
 from fastapi import HTTPException, APIRouter, Depends, Cookie, Response, BackgroundTasks
 from sqlalchemy.orm import Session
+
+from core.story_generator import StoryGenerator
 from db.database import get_db, SessionLocal
 from models.story import Story,StoryNode
 from models.job import StoryJob
@@ -61,8 +63,9 @@ def generate_story_task(job_id: str,theme: str, session_id: str):
             job.status = "processing"
             db.commit()
 
-            story = {} #to_do generate story
-            job.story_id = 1 #to_do update story id
+            story = StoryGenerator.generate_story(db, session_id, theme)
+
+            job.story_id = story.id #to_do update story id
 
 
             job.status = "completed"
@@ -87,4 +90,28 @@ def get_complete_story(story_id: int, db: Session = Depends(get_db)):
     return story
 
 def build_complete_story_tree(db: Session, story: Story) -> CompleteStoryResponse:
-    pass
+    nodes = db.query(StoryNode).filter(StoryNode.story_id == story.id).all()
+
+    node_dict = {}
+    for node in nodes:
+        node_response = CompleteStoryNodeResponse(
+            id=node.id,
+            content=node.content,
+            is_ending=node.is_ending,
+            is_winning_ending=node.is_winning_ending,
+            options=node.options
+        )
+        node_dict[node.id] = node_response
+
+    root_node = next((node for node in nodes if node.is_root), None)
+    if not root_node:
+        raise HTTPException(status_code=500, detail="Story root node not found")
+
+    return CompleteStoryResponse(
+        id=story.id,
+        title= story.title,
+        session_id=story.session_id,
+        created_at=story.created_at,
+        root_node=node_dict[root_node.id],
+        all_nodes=node_dict
+    )
