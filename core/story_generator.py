89489af05317e31babf18375b8e72ed8066fffc7
deepinder-gill <deepinderrgilll@gmail.com>
2026-03-07 02:46:43 +0530

    Update story_generator.py

---
+++ core/story_generator.py
@@ -0,0 +1,12 @@
+from sqlalchemy.orm import Session
+
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
+
+from core.prompts import STORY_PROMPT
+from models.story import Story, StoryNode
+from core.models import StoryLLMResponse, StoryNodeLLM
+from dotenv import load_dotenv
+import os
+
